48bc2db11a4d4c7b9b820bd7768893c40c76f61d
VíctorBlasco5 <victor.blasco.17@gmail.com>
2024-02-26 19:59:59 +0100

    express server running

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import express, { Request, Response } from "express";
+const app = express();
+
+app.listen(3000,()=>console.log('Servidor levantado en 3000'));
+
+app.get('/',(req:Request,res:Response)=>{
+    res.send('Hello world!')
+    });
